bfedd4175181aae3ab036c27659137f6f7ccbc37
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-03-01 08:25:12 -0600

    Example jquery, commented out remote module usage

--- el-basic/src/renderer.js
+++ el-basic/src/renderer.js
@@ -16,3 +16,34 @@ redBlock.style.background = 'red'
 
 container.append(blueBlock)
 container.append(redBlock)
+
+// Playing around with jquery
+const $ = require('jquery')
+
+$('#container').css('border', '3px solid red')
+
+let count = 0
+
+let counterButton = $('<button>Click</button>')
+counterButton.on('click', () => {
+  count += 1
+  $('.count-container').remove()
+  $('body').append(`<div class='count-container'>${count}</div>`)
+})
+
+$('body').append(counterButton)
+
+// Open a dialog menu
+// There is discussion on removing the remote module from electron with suggestions to use IPC processes instead
+//
+// const { remote } = require('electron')
+
+// const dialog = remote.dialog
+
+// let dialogButton = $('<button>Dialog</button>')
+// dialogButton.on('click', () => {
+//   dialog.showOpenDialog()
+// })
+
+// $('body').append(dialogButton)
+
