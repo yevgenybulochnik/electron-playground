1159dbbfab3093ebed54f114234e0ef552e00a19
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-03-22 20:40:08 -0500

    Nav sidebar setup

--- el-cra-sqlite/src/components/NavBar.js
+++ el-cra-sqlite/src/components/NavBar.js
@@ -3,9 +3,18 @@ import { fade, makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
 import Typography from '@material-ui/core/Typography'
 import CloseIcon from '@material-ui/icons/Close'
+import Drawer from '@material-ui/core/Drawer'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import PersonAddIcon from '@material-ui/icons/PersonAdd'
+import GroupAddIcon from '@material-ui/icons/GroupAdd'
+import AccountBox from '@material-ui/icons/AccountBox'
+
 
 const electron = window.require('electron')
 const ipcRenderer = electron.ipcRenderer
@@ -15,10 +24,26 @@ function closeApp() {
   ipcRenderer.send('close-app')
 }
 
+const drawerWidth = 180 
+
 const useStyles = makeStyles((theme) => ({
   grow: {
     flex: 1,
-  }
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    WebkitAppRegion: 'drag',
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolBar: {
+    height: '48px',
+  },
 }))
 
 export default function NavBar() {
@@ -26,24 +51,57 @@ export default function NavBar() {
   const classes = useStyles()
 
   return (
-    <AppBar>
-      <Toolbar variant="dense">
-        <IconButton
-          edge="start"
-          color="inherit"
-        >
-          <MenuIcon></MenuIcon>
-        </IconButton>
-        <Typography className={classes.grow}>
-          Electron CRA Sqlite
-        </Typography>
-        <IconButton
-          color="inherit"
-          onClick={closeApp}
-        >
-          <CloseIcon/>
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar className={classes.appBar}>
+        <Toolbar variant="dense">
+          <Typography className={classes.grow}>
+            Electron CRA Sqlite
+          </Typography>
+          <IconButton
+            color="inherit"
+            onClick={closeApp}
+          >
+            <CloseIcon/>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant='permanent'
+        anchor='left'
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.toolBar} />
+        <Divider />
+        <List>
+          <ListItem
+            button
+          >
+            <ListItemIcon>
+              <AccountBox/>
+            </ListItemIcon>
+            <ListItemText primary='Views'/>
+          </ListItem>
+          <ListItem
+            button
+          >
+            <ListItemIcon>
+              <PersonAddIcon/>
+            </ListItemIcon>
+            <ListItemText primary='Add Users'/>
+          </ListItem>
+          <ListItem
+            button
+          >
+            <ListItemIcon>
+              <GroupAddIcon/>
+            </ListItemIcon>
+            <ListItemText primary='Add Group'/>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
   )
 }
