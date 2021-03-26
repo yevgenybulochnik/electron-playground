d7ae6786d2ae782ddfc510e05a3e7a10bd24678c
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-03-26 08:42:38 -0500

    Initial groupcard component

---
+++ el-cra-sqlite/src/components/GroupCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+}))
+
+export default function UserCard() {
+  const classes = useStyles()
+
+  return (
+    <div>
+    </div>
+  )
+}
