e938702088b56d822b047a18467d1cd285277d8c
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-04-04 17:37:55 -0500

    Basic create user form

--- el-cra-sqlite/src/views/AddUser.js
+++ el-cra-sqlite/src/views/AddUser.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: '30px 10px 10px 10px',
+  },
+  header: {
+    marginBottom: 10,
+  },
+  paper: {
+    padding: 20,
+  },
+  inputs: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+  },
+  submit: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+}))
 
 export default function AddUserView() {
+  const classes = useStyles()
   return (
-    <div>
-      Add User
+    <div className={classes.root}>
+      <Paper elevation={3} className={classes.paper}>
+        <Typography className={classes.header}>
+          Create New User
+        </Typography>
+        <div className={classes.inputs}>
+          <TextField
+            label='Username'
+            helperText='Enter a new Username'
+            variant='outlined'
+          />
+          <TextField
+            label='Email'
+            helperText='Enter an email'
+            variant='outlined'
+          />
+        </div>
+        <div className={classes.submit}>
+          <Button
+            variant='contained'
+            color='primary'
+          >
+            Create
+          </Button>
+        </div>
+      </Paper>
     </div>
   )
 }
